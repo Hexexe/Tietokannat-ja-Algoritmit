c331edfdab06153dc2ba4e39d5c751ec7e666007
Hexexe <TusernameAken@gmail.com>
2020-04-21 08:51:27 +0300

    why contain it

--- src/Tehtävä_6_LinkedList/Stack.java
+++ src/Tehtävä_6_LinkedList/Stack.java
@@ -9,14 +9,22 @@ public class Stack {
     public Stack() {
         list = new LinkedList<>();
     }
+
     // Muodosta uusi lista-alkio ja vie se pinon huipulle
-    public void push(String aData) { list.add(new ListItem(aData)); }
+    public void push(String aData) {
+        list.add(new ListItem(aData));
+    }
+
     // tarkista onko lista tyhjä. jos ei niin purge viimeinen listalta
-    public ListItem pop() { return list.size() > 0 ? list.removeLast() : null; }
+    public ListItem pop() {
+        return list.size() > 0 ? list.removeLast() : null;
+    }
+
     // Palauta pinon koko
     public int getSize() {
         return list.size();
     }
+
     //Tulosta pinon sisältö muuttamatta pinoa
     public void printItems() {
         Iterator<ListItem> iterator = list.descendingIterator();
