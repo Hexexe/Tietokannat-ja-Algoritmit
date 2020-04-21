8d551c6a0f1cda3c85b0efe0cc7e3dd6da6055c2
Hexexe <TusernameAken@gmail.com>
2020-04-21 08:50:38 +0300

    why contain it

--- src/Tehtävä_6_LinkedList/Stack.java
+++ src/Tehtävä_6_LinkedList/Stack.java
@@ -8,19 +8,19 @@ public class Stack {
 
     public Stack() {
         list = new LinkedList<>();
-    } // constructor
-
-    public void push(String aData) { list.add(new ListItem(aData)); } // Muodosta uusi lista-alkio ja vie se pinon huipulle
-
-    public ListItem pop() { return list.size() > 0 ? list.removeLast() : null; } // tarkista onko lista tyhjä. jos ei niin purge viimeinen listalta
-
+    }
+    // Muodosta uusi lista-alkio ja vie se pinon huipulle
+    public void push(String aData) { list.add(new ListItem(aData)); }
+    // tarkista onko lista tyhjä. jos ei niin purge viimeinen listalta
+    public ListItem pop() { return list.size() > 0 ? list.removeLast() : null; }
+    // Palauta pinon koko
     public int getSize() {
         return list.size();
-    } // Palauta pinon koko
-
+    }
+    //Tulosta pinon sisältö muuttamatta pinoa
     public void printItems() {
         Iterator<ListItem> iterator = list.descendingIterator();
-        while (iterator.hasNext()) System.out.println(iterator.next()); //Tulosta pinon sisältö muuttamatta pinoa
+        while (iterator.hasNext()) System.out.println(iterator.next());
     }
 
 
